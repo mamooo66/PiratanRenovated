9d7b58e5f5d509778b7e28cf47d251fc7db190b5
Mahmud İçme <mahmudicme54@gmail.com>
2022-12-28 23:36:45 +0300

    v0.0.5

--- Assets/Scripts/Game/Manager/PlayerManager.cs
+++ Assets/Scripts/Game/Manager/PlayerManager.cs
@@ -1,33 +1,118 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.AI;
 using Mirror;
+using UnityEngine.PlayerLoop;
 
 public class PlayerManager : NetworkBehaviour
 {
+    private Vector3 difference;
+    private Vector3 dragOrigin;
+    private bool dragPressed;
+    private bool dragEnough;
+    
+    [Header("Movement")]
     [SerializeField] private NavMeshAgent agent;
+    RaycastHit hit;
+    public Joystick moveJoystick;
+    public Transform ship;
+    public bool pressMoveJoystick;
+    
+    [Header("Camera")]
+    public Joystick cameraJoystick;
+    public bool pressCameraJoystick;
+    public bool focusCamera;
 
     private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
         if (isLocalPlayer)
         {
             transform.GetChild(0).gameObject.SetActive(true);
         }
     }
-
-    public void dragAreaInputUp()
+    
+    void FixedUpdate()
     {
-        Debug.Log("Basıldı Client");
-        ShipMove(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (isLocalPlayer)
+        {
+            if (pressMoveJoystick)//Move Joysticke basılı tutulduğu sürece hareket fonksiyonuna veri gönderir
+            {
+                Vector3 direction = new Vector3(moveJoystick.Horizontal, 0, moveJoystick.Vertical);
+                shipMoveCMD(direction + transform.position);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (isLocalPlayer)
+        {
+            if (pressCameraJoystick)
+            {
+                float cameraSizeRatio = Camera.main.orthographicSize / 25f;
+                CameraCommands.CameraMove(new Vector3(cameraJoystick.Horizontal * 0.15f * cameraSizeRatio, 0, cameraJoystick.Vertical * 0.15f * cameraSizeRatio));
+            }
+            else if(focusCamera)
+            {
+                CameraCommands.CameraMoveToPlayer(transform);
+            }
+        }
+    }
+
+    public void dragAreaInputUp() 
+    {
+        NavMeshHit navMeshHit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition) ,out hit,  Mathf.Infinity)){
+            NavMesh.SamplePosition(hit.point, out navMeshHit, 65, -1);
+            shipMoveCMD(navMeshHit.position);
+        }
     }
     
     [Command]
-    public void ShipMove(Ray ray)
+    public void shipMoveCMD(Vector3 targetPos) //Harita üzerinde kaydırma yapılmadan tıklanılan yere gidilmesi içindir. Bu fonsksiyon server üzerinde çalışır. MoveCommands'a hareket isteği gönderilir.
     {
-        Debug.Log("Basıldı Server");
-        MoveCommands.MoveToPos(ray, agent);
+        MoveCommands.MoveToPos(targetPos, agent);
+        RotateShip(targetPos);
+    }
+    
+    [ClientRpc]
+    public void RotateShip(Vector3 targetPos) //Gemi gittiği yöne doğru yönelir
+    {
+        MoveCommands.Rotate(targetPos, transform.position, ship);
+    }
+
+    [Command]
+    public void moveJoystickDownCMD() //Joysticke basıldığında hali hazırda gidilen path sıfırlanır
+    {
+        MoveCommands.Stop(agent);
+    }
+    
+    public void moveJoystickDown()
+    {
+        pressMoveJoystick = true;
+    }
+    
+    public void moveJoystickUp()
+    {
+        pressMoveJoystick = false;
+    }
+    
+    public void cameraJoystickDown()
+    {
+        focusCamera = false;
+        pressCameraJoystick = true;
+    } 
+    
+    public void cameraJoystickUp()
+    {
+        pressCameraJoystick = false;
+    }
+    
+    public void focusShipButton()
+    {
+        focusCamera = true;
     }
 }
--- Assets/Scripts/Game/ShipMechanic/MoveCommands.cs
+++ Assets/Scripts/Game/ShipMechanic/MoveCommands.cs
@@ -13,24 +13,19 @@ public class MoveCommands : NetworkBehaviour
     // Seçilen bir noktaya hareket etme
     // Hareket ederken rotasyonu değiştirme
     
-    public static void MoveToPos(Ray mousePos, NavMeshAgent agent){
-        RaycastHit hit;
-        NavMeshHit navMeshHit;
-        if(Physics.Raycast(mousePos,out hit,  Mathf.Infinity)){
-            NavMesh.SamplePosition(hit.point, out navMeshHit, 65, -1);
-            agent.SetDestination(navMeshHit.position);
-            Debug.Log(agent.destination);
-        }
+    public static void MoveToPos(Vector3 movePos, NavMeshAgent agent) //
+    {
+        agent.SetDestination(movePos);
     }
-
-    public static Vector3 Rotate(Vector3 movePos, Vector3 pos){
-        var dir = movePos - pos;
+    
+    public static void Stop(NavMeshAgent agent){
+        agent.ResetPath();
+    }
+    
+    public static void Rotate(Vector3 targetPos, Vector3 shipPos, Transform shipTransform){
+        var dir = targetPos - shipPos;
         Quaternion LookAtRotation = Quaternion.LookRotation(dir);
         Quaternion LookAtRotationOnly_Y = Quaternion.Euler(0, LookAtRotation.eulerAngles.y + 180, 0);
-        return LookAtRotationOnly_Y.eulerAngles;
-    }
-
-    public static void Stop(NavMeshAgent agent){
-        agent.SetDestination(agent.transform.position);
+        shipTransform.eulerAngles = LookAtRotationOnly_Y.eulerAngles;
     }
 }
